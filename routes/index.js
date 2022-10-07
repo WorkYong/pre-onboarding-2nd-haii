124bbd124a00ea55de8145353ac576e910a20644
전준영 <wnsdud9476@DESKTOP-2VICEL1.localdomain>
2022-10-07 19:42:22 +0900

    [MODIFY] init 수정

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const userController = require("../controllers/userController");
+const userRouter = require("../routes/userRouter");
 
 const router = express.Router();
+
+router.use("/users", userRouter);
 
 module.exports = router;
